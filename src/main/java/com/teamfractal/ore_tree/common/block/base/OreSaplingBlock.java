df7c650b3f16a5bdd8972688f65c0f2f6dee7e8b
MikhailTapio <mikhailtapio13468@163.com>
2021-12-17 17:29:57 +0800

    Backport to 1.17.1.

---
+++ src/main/java/com/teamfractal/ore_tree/common/block/base/OreSaplingBlock.java
@@ -0,0 +1,152 @@
+package com.teamfractal.ore_tree.common.block.base;
+
+import com.teamfractal.ore_tree.client.config.OTClientConfig;
+import net.minecraft.client.multiplayer.ClientLevel;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.tags.Tag;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.LevelReader;
+import net.minecraft.world.level.LightLayer;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.BushBlock;
+import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.IntegerProperty;
+import net.minecraft.world.level.material.Material;
+import net.minecraft.world.level.storage.loot.LootContext;
+import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.VoxelShape;
+
+import javax.annotation.Nonnull;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
+import static com.teamfractal.ore_tree.common.action.OreTreePlacement.consumeCatalyst;
+import static com.teamfractal.ore_tree.common.action.OreTreePlacement.tryPlace;
+
+public class OreSaplingBlock extends BushBlock{
+    private static final IntegerProperty AGE = BlockStateProperties.AGE_3;
+    private static final VoxelShape SHAPE = Block.box(2.0D, 0.0D, 2.0D, 14.0D, 12.0D, 14.0D);
+    private final Item catalystItem;
+    private final Tag<Block> rootable;
+    private final double randomTickValue;
+    private final double randomCatalyzeValue;
+    private final ResourceLocation treeNBTLocation;
+    public OreSaplingBlock(Item catalystItem, Tag<Block> rootable, double randomTickValue, double randomCatalyzeValue, ResourceLocation treeNBTLocation) {
+        super(Properties.of(Material.PLANT).randomTicks().noCollission().sound(SoundType.GRASS).instabreak());
+        this.catalystItem = catalystItem;
+        this.randomTickValue = randomTickValue;
+        this.randomCatalyzeValue = randomCatalyzeValue;
+        this.treeNBTLocation = treeNBTLocation;
+        this.rootable = rootable;
+        this.registerDefaultState(this.stateDefinition.any().setValue(AGE, 0));
+    }
+
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+        builder.add(AGE);
+    }
+
+    public BlockState getStateForPlacement(@Nonnull BlockPlaceContext p_48881_) {
+        return this.defaultBlockState().setValue(AGE, 0);
+    }
+
+    @Nonnull
+    public VoxelShape getShape(@Nonnull BlockState state, @Nonnull BlockGetter getter, @Nonnull BlockPos pos, @Nonnull CollisionContext cc) {
+        return SHAPE;
+    }
+
+    @Override
+    public int getFlammability(BlockState state, BlockGetter world, BlockPos pos, Direction face) {
+        return 100;
+    }
+
+    @Override
+    public int getFireSpreadSpeed(BlockState state, BlockGetter world, BlockPos pos, Direction face) {
+        return 60;
+    }
+
+    @Nonnull
+    @Override
+    public List<ItemStack> getDrops(@Nonnull BlockState state, @Nonnull LootContext.Builder builder) {
+        List<ItemStack> dropsOriginal = super.getDrops(state, builder);
+        if (!dropsOriginal.isEmpty())
+            return dropsOriginal;
+        return Collections.singletonList(new ItemStack(this, 1));
+    }
+
+    @Override
+    public boolean mayPlaceOn(BlockState groundState, @Nonnull BlockGetter worldIn, @Nonnull BlockPos pos) {
+        return groundState.is(rootable);
+    }
+
+    @Override
+    public boolean canSurvive(@Nonnull BlockState blockstate, @Nonnull LevelReader worldIn, BlockPos pos) {
+        return this.mayPlaceOn(worldIn.getBlockState(pos.below()), worldIn, pos.below());
+    }
+
+    @Nonnull
+    public ItemStack getCloneItemStack(@Nonnull BlockGetter p_52254_, @Nonnull BlockPos p_52255_, @Nonnull BlockState p_52256_) {
+        return new ItemStack(this);
+    }
+
+    public void randomTick(BlockState state, @Nonnull ServerLevel world, @Nonnull BlockPos pos, @Nonnull Random random) {
+        int i = state.getValue(AGE);
+
+        if(random.nextDouble() <= randomTickValue && world.getBrightness(LightLayer.SKY, pos.above()) - world.getSkyDarken() >= 9)
+        {
+            if (i < 3) {
+                world.setBlock(pos, state.setValue(AGE, i + 1), 2);
+            }else{
+                tryPlace(world,treeNBTLocation,pos);
+            }
+        }
+    }
+
+    @Nonnull
+    public InteractionResult use(@Nonnull BlockState state, @Nonnull Level level, @Nonnull BlockPos pos, @Nonnull Player player, @Nonnull InteractionHand hand, @Nonnull BlockHitResult trace) {
+        int i = state.getValue(AGE);
+        Random random = new Random();
+        if(player.getItemInHand(hand).getItem() == catalystItem){
+            if (!(i >= 3)) {
+
+                if(random.nextDouble() <= randomCatalyzeValue) {
+                    if(level instanceof ServerLevel serverLevel)
+                    {
+                        serverLevel.sendParticles(ParticleTypes.HAPPY_VILLAGER, pos.getX() + 0.5, pos.getY() + 0.7, pos.getZ() + 0.5, 5, 0, 0, 0, 1);
+                        serverLevel.setBlock(pos, state.setValue(AGE, i + 1), 2);
+                    }
+                }
+                else if(OTClientConfig.CATALYZE_FAILURE_PARTICLE_DISPLAY.get()){
+                    if(level instanceof ClientLevel clientLevel){
+                        clientLevel.addParticle(ParticleTypes.LARGE_SMOKE, pos.getX() + 0.5, pos.getY() + 0.7, pos.getZ() + 0.5, 0, 0, 0);
+                    }
+                }
+
+                consumeCatalyst(player, catalystItem.getDefaultInstance());
+
+            }else{
+                if(tryPlace(level,treeNBTLocation,pos)) {
+                    consumeCatalyst(player, catalystItem.getDefaultInstance());
+                }
+            }
+        }else {
+            return super.use(state, level, pos, player, hand, trace);
+        }
+        return InteractionResult.PASS;
+    }
+}
---
+++ src/main/java/com/teamfractal/ore_tree/common/block/init/OTBlocks.java
@@ -0,0 +1,77 @@
+package com.teamfractal.ore_tree.common.block.init;
+
+import com.teamfractal.ore_tree.common.block.base.OreInlaidLogBlock;
+import com.teamfractal.ore_tree.common.block.base.OreSaplingBlock;
+import com.teamfractal.ore_tree.common.config.OTCommonConfig;
+import com.teamfractal.ore_tree.common.util.OTRegistryHandler;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.tags.Tag;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.level.block.Block;
+import net.minecraftforge.fmllegacy.RegistryObject;
+
+import static net.minecraft.tags.BlockTags.bind;
+
+public class OTBlocks {
+
+    public static final Tag.Named<Block> QUARTZ_ORES = bind("quartz_ores");
+
+    public static RegistryObject<Block> coalTreeLog;
+    public static RegistryObject<Block> copperTreeLog;
+    public static RegistryObject<Block> diamondTreeLog;
+    public static RegistryObject<Block> emeraldTreeLog;
+    public static RegistryObject<Block> goldTreeLog;
+    public static RegistryObject<Block> ironTreeLog;
+    public static RegistryObject<Block> lapisTreeLog;
+    public static RegistryObject<Block> quartzTreeLog;
+    public static RegistryObject<Block> redstoneTreeLog;
+    public static RegistryObject<Block> coalTreeSapling;
+    public static RegistryObject<Block> copperTreeSapling;
+    public static RegistryObject<Block> diamondTreeSapling;
+    public static RegistryObject<Block> emeraldTreeSapling;
+    public static RegistryObject<Block> goldTreeSapling;
+    public static RegistryObject<Block> ironTreeSapling;
+    public static RegistryObject<Block> lapisTreeSapling;
+    public static RegistryObject<Block> quartzTreeSapling;
+    public static RegistryObject<Block> redstoneTreeSapling;
+
+    public static void register(){
+        coalTreeLog = OTRegistryHandler.Blocks.register("coal_tree_log", OreInlaidLogBlock::new);
+        copperTreeLog = OTRegistryHandler.Blocks.register("copper_tree_log", OreInlaidLogBlock::new);
+        diamondTreeLog = OTRegistryHandler.Blocks.register("diamond_tree_log", OreInlaidLogBlock::new);
+        emeraldTreeLog = OTRegistryHandler.Blocks.register("emerald_tree_log", OreInlaidLogBlock::new);
+        goldTreeLog = OTRegistryHandler.Blocks.register("gold_tree_log", OreInlaidLogBlock::new);
+        ironTreeLog = OTRegistryHandler.Blocks.register("iron_tree_log", OreInlaidLogBlock::new);
+        lapisTreeLog = OTRegistryHandler.Blocks.register("lapis_tree_log", OreInlaidLogBlock::new);
+        quartzTreeLog = OTRegistryHandler.Blocks.register("quartz_tree_log", OreInlaidLogBlock::new);
+        redstoneTreeLog = OTRegistryHandler.Blocks.register("redstone_tree_log", OreInlaidLogBlock::new);
+        coalTreeSapling = OTRegistryHandler.Blocks.register("coal_tree_sapling"
+                ,() -> new OreSaplingBlock(Items.COAL, BlockTags.COAL_ORES, OTCommonConfig.COAL_TICK.get(), OTCommonConfig.COAL_CATALYZE.get(), new ResourceLocation("ore_tree", "coal_tree"))
+        );
+        copperTreeSapling = OTRegistryHandler.Blocks.register("copper_tree_sapling"
+                ,() -> new OreSaplingBlock(Items.COPPER_INGOT, BlockTags.COPPER_ORES, OTCommonConfig.COPPER_TICK.get(), OTCommonConfig.COPPER_CATALYZE.get(), new ResourceLocation("ore_tree", "copper_tree"))
+        );
+        diamondTreeSapling = OTRegistryHandler.Blocks.register("diamond_tree_sapling"
+                ,() -> new OreSaplingBlock(Items.DIAMOND, BlockTags.DIAMOND_ORES, OTCommonConfig.DIAMOND_TICK.get(), OTCommonConfig.DIAMOND_CATALYZE.get(), new ResourceLocation("ore_tree", "diamond_tree"))
+        );
+        emeraldTreeSapling = OTRegistryHandler.Blocks.register("emerald_tree_sapling"
+                ,() -> new OreSaplingBlock(Items.EMERALD, BlockTags.EMERALD_ORES, OTCommonConfig.EMERALD_TICK.get(), OTCommonConfig.EMERALD_CATALYZE.get(), new ResourceLocation("ore_tree", "emerald_tree"))
+        );
+        goldTreeSapling = OTRegistryHandler.Blocks.register("gold_tree_sapling"
+                ,() -> new OreSaplingBlock(Items.GOLD_INGOT, BlockTags.GOLD_ORES, OTCommonConfig.GOLD_TICK.get(), OTCommonConfig.GOLD_CATALYZE.get(), new ResourceLocation("ore_tree", "gold_tree"))
+        );
+        ironTreeSapling = OTRegistryHandler.Blocks.register("iron_tree_sapling"
+                ,() -> new OreSaplingBlock(Items.IRON_INGOT, BlockTags.IRON_ORES, OTCommonConfig.IRON_TICK.get(), OTCommonConfig.IRON_CATALYZE.get(), new ResourceLocation("ore_tree", "iron_tree"))
+        );
+        lapisTreeSapling = OTRegistryHandler.Blocks.register("lapis_tree_sapling"
+                ,() -> new OreSaplingBlock(Items.LAPIS_LAZULI, BlockTags.LAPIS_ORES, OTCommonConfig.LAPIS_TICK.get(), OTCommonConfig.LAPIS_CATALYZE.get(), new ResourceLocation("ore_tree", "lapis_tree"))
+        );
+        quartzTreeSapling = OTRegistryHandler.Blocks.register("quartz_tree_sapling"
+                ,() -> new OreSaplingBlock(Items.QUARTZ, QUARTZ_ORES, OTCommonConfig.QUARTZ_TICK.get(), OTCommonConfig.QUARTZ_CATALYZE.get(), new ResourceLocation("ore_tree", "quartz_tree"))
+        );
+        redstoneTreeSapling = OTRegistryHandler.Blocks.register("redstone_tree_sapling"
+                ,() -> new OreSaplingBlock(Items.REDSTONE, BlockTags.REDSTONE_ORES, OTCommonConfig.REDSTONE_TICK.get(), OTCommonConfig.REDSTONE_CATALYZE.get(), new ResourceLocation("ore_tree", "redstone_tree"))
+        );
+    }
+}
