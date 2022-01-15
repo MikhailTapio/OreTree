588df7b5e0e64b3a9ec4169fec6a77b7e8b5a56b
MikhailTapio <mikhailtapio13468@163.com>
2022-01-15 10:48:57 +0800

    Initially port to 1.17.x.

---
+++ src/main/java/com/teamfractal/ore_tree/common/OreTree.java
@@ -0,0 +1,19 @@
+package com.teamfractal.ore_tree.common;
+
+import com.teamfractal.ore_tree.common.block.OTBlocks;
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.Identifier;
+
+public class OreTree implements ModInitializer {
+    public static final String N = "ore_tree";
+    public static final ItemGroup OreTree = FabricItemGroupBuilder.build(
+            new Identifier("ore_tree", "main"),
+            () -> new ItemStack(OTBlocks.diamondTreeLog));
+    @Override
+    public void onInitialize() {
+        OTBlocks.register();
+    }
+}
